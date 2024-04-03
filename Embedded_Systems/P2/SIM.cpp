9108e7055ef1bc3c40935c021ab4ace2a57628ed
Kyle-912 <kdt912@outlook.com>
2024-04-03 21:01:21 +0000

    Apr 3, 2024, 5:01 PM

--- Embedded_Systems/P2/SIM.cpp
+++ Embedded_Systems/P2/SIM.cpp
@@ -71,6 +71,10 @@ void compression()
     // Mismatches Handling
     for (size_t i = 0; i < instructions.size(); ++i)
     {
+        if (instructions[i].size() != 32)
+        {
+            continue; // Skip instructions that are not 32 bits
+        }
         const string &instr = instructions[i];
         for (size_t j = 0; j < dictionaryEntries.size(); ++j)
         {
