a85d852c6744e847f32a6097bee238e1fb6910a8
Kyle-912 <kdt912@outlook.com>
2024-04-03 22:39:59 +0000

    Apr 3, 2024, 6:39 PM

--- Embedded_Systems/P2/SIM.cpp
+++ Embedded_Systems/P2/SIM.cpp
@@ -89,7 +89,7 @@ void compression()
             }
             for (size_t k = 0; k < instr.size(); ++k)
             {
-                if (k == 31)
+                if (k == 30)
                 {
                     cout << "";
                 }
