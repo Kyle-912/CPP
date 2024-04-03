7ec8b9a5b779c9ae5d9a4f15af02d5f953c774d1
Kyle-912 <kdt912@outlook.com>
2024-04-03 00:19:23 +0000

    Apr 2, 2024, 8:19 PM

--- Embedded_Systems/P2/SIM.cpp
+++ Embedded_Systems/P2/SIM.cpp
@@ -1,9 +1,31 @@
 // “I have neither given nor received any unauthorized aid on this assignment
 
 #include <iostream>
+#include <cstdlib>
 
-int main()
+int main(int argc, char *argv[])
 {
-    std::cout << "Hello, world!" << std::endl;
+    if (argc != 2)
+    {
+        std::cerr << "Usage: " << argv[0] << " <1|2>" << std::endl;
+        return 1;
+    }
+
+    int option = std::atoi(argv[1]);
+
+    if (option == 1)
+    {
+        std::cout << "Option 1 selected. Hello!" << std::endl;
+    }
+    else if (option == 2)
+    {
+        std::cout << "Option 2 selected. World!" << std::endl;
+    }
+    else
+    {
+        std::cerr << "Invalid option. Please provide either 1 or 2." << std::endl;
+        return 1;
+    }
+
     return 0;
 }
