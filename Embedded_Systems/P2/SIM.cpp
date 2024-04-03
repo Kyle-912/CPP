cfd3df0efa4ba06f7d43d670570b5d0a67fef80a
Kyle-912 <kdt912@outlook.com>
2024-04-03 00:36:30 +0000

    Apr 2, 2024, 8:36 PM

--- Embedded_Systems/P2/SIM.cpp
+++ Embedded_Systems/P2/SIM.cpp
@@ -1,6 +1,7 @@
 // “I have neither given nor received any unauthorized aid on this assignment
 
 #include <iostream>
+#include <fstream>
 #include <cstdlib>
 
 using namespace std;
@@ -11,7 +12,22 @@ int main(int argc, char *argv[])
 
     if (option == 1)
     {
-        cout << "Option 1 selected. Hello!" << endl;
+        ifstream file("original.txt"); // Open the file for reading
+
+        if (file.is_open())
+        {
+            cout << "Contents of original.txt:" << endl;
+            char c;
+            while (file.get(c)) // Read the file character by character
+            {
+                cout << c;
+            }
+            file.close(); // Close the file
+        }
+        else
+        {
+            cerr << "Error opening file original.txt" << endl;
+        }
     }
     else if (option == 2)
     {
