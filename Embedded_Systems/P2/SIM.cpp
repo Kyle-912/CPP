c4833a41b478c2e736223d3fb4156b083f5c7a3f
Kyle-912 <kdt912@outlook.com>
2024-04-03 00:21:11 +0000

    Apr 2, 2024, 8:21 PM

--- Embedded_Systems/P2/SIM.cpp
+++ Embedded_Systems/P2/SIM.cpp
@@ -7,15 +7,15 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    int option = std::atoi(argv[1]);
+    int option = atoi(argv[1]);
 
     if (option == 1)
     {
-        std::cout << "Option 1 selected. Hello!" << std::endl;
+        cout << "Option 1 selected. Hello!" << endl;
     }
     else if (option == 2)
     {
-        std::cout << "Option 2 selected. World!" << std::endl;
+        cout << "Option 2 selected. World!" << endl;
     }
 
     return 0;
