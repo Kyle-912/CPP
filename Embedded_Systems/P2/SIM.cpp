a1f854d76853030c06ec5c03f4a5fca0870d414f
Kyle-912 <kdt912@outlook.com>
2024-04-03 00:57:14 +0000

    Apr 2, 2024, 8:57 PM

--- Embedded_Systems/P2/SIM.cpp
+++ Embedded_Systems/P2/SIM.cpp
@@ -10,11 +10,6 @@ void readFile(int selection);
 
 int main(int argc, char *argv[])
 {
-    if (argc != 2)
-    {
-        return 1;
-    }
-
     int selection = atoi(argv[1]);
     readFile(selection);
 
